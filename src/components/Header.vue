3bb076401871101d81d1e4b80b5caa44c3a33ace
Iurii <ghostrider.gu@gmail.com>
2022-02-10 21:41:05 +0300

    update the base of app

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <strong>Verbarium</strong>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    userName: String,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  color: cadetblue;
+}
+</style>
